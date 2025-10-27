56d768ffe14c96a5ad768507fa1ce88e4d8583ca
hbrls <shuaizhexu@gmail.com>
2025-10-27 11:27:35 +0800

    !: docs

--- packages/rum/src/boot/rumPublicApi.js
+++ packages/rum/src/boot/rumPublicApi.js
@@ -54,7 +54,7 @@ export function makeRumPublicApi(startRumImpl, recorderApi, options) {
   let strategy = createPreStartStrategy(
     options,
     getCommonContext,
-    function (configuration, deflateWorker, initialViewOptions) {
+    function doStartRum(configuration, deflateWorker, initialViewOptions) {
       if (configuration.storeContextsToLocal) {
         storeContextManager(
           configuration,
